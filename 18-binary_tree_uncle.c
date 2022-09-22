d0bbc83b1276dfc12214edd325840c4224c6d758
codestronomer <johnrumide6@gmail.com>
2022-09-22 08:33:32 +0100

    [fixes]

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -11,19 +11,18 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 	binary_tree_t *parent;
 	binary_tree_t *grandparent;
 
-	if (node == NULL)
+	if (node == NULL || node->parent == NULL)
 		return (NULL);
 
 	parent = node->parent;
 	grandparent = parent->parent;
 
-	if (parent != NULL && grandparent != NULL)
+	if (grandparent != NULL)
 	{
 		if (grandparent->left && grandparent->left != parent)
 			return (grandparent->left);
 		if (grandparent->right && grandparent->right != parent)
 			return (grandparent->right);
-		return (NULL);
 	}
 	return (NULL);
 }
